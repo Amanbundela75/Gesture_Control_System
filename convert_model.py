b60f88211ea88576dac55bc4d2c2b9046757cb3a
Anjana Dhakad <anjanadhakad9584@gmail.com>
2025-09-03 23:49:42 +0530

    chore: Update model filename in conversion script

--- convert_model.py
+++ convert_model.py
@@ -1,18 +1,15 @@
 import tensorflow as tf
 
-# Apne Keras model (.h5 file) ko load karein
-model = tf.keras.models.load_model('hand_gesture_model.h5')
-print("H5 model loaded successfully.")
+# Naye model (.h5 file) ka naam yahan daalein
+model_name = 'hand_gesture_model_asl.h5'
 
-# TFLite converter banayein
+model = tf.keras.models.load_model(model_name)
+print(f"'{model_name}' loaded successfully.")
+
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
-
-# Conversion process run karein
 tflite_model = converter.convert()
 print("Model converted to TensorFlow Lite format.")
 
-# Naye .tflite model ko save karein
 with open('model.tflite', 'wb') as f:
     f.write(tflite_model)
-
 print("New model saved as 'model.tflite'. ✅")
